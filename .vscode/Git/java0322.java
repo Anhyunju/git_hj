99d8965355dd4d9a8d82b19e8114d0bf1ea853fe
sample <sample@naver.com>
2022-03-23 09:01:33 +0900

    commit

--- .vscode/Git/java0322.java
+++ .vscode/Git/java0322.java
@@ -1,16 +1,10 @@
-package .vscode.Git;
-/**
- * java0322
- */
-public class java0322 {
-    public static void main(String[] args) {
-        System.out.println("
-            깃허브 commit 연동 
+/*
+            깃허브 commit 연동
             git init 
             git add . --모든파일 업로드
             git add java0322.java --java0322파일만 업로드
             git status --바뀐부분 확인
-            git commit -m "commit msg" -- 커밋 메세지
+            git commit -m 'commit msg' -- 커밋 메세지
             git remote add origin https://github.com/Anhyunju/git_hj.git -- 경로설정
             git push origin master --업로드
 
@@ -24,7 +18,4 @@ public class java0322 {
             git branch -M main/master
             git push origin main/master
                  
-        ");
-    }
-    
-}
+*/
