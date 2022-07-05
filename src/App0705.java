a30156f3ee32d15f8dce8a25a7f1a9a6520f756a
test <test@gmail.com>
2022-07-05 15:09:11 +0900

    m7

--- src/App0705.java
+++ src/App0705.java
@@ -3,7 +3,7 @@ public class App0705 {
         int count= 10;
         System.out.println(count);
 
-        final double PI = 3.14159;
+        final double PI = 3.141599;
         int j = 5;
         double rs = j * 2 * PI;
         System.out.println(rs);
