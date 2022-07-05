65cc9805fdae48d72d3ea0277df4630fb954c73a
test <test@gmail.com>
2022-07-05 14:35:21 +0900

    ss

---
+++ src/App0705.java
@@ -0,0 +1,14 @@
+public class App0705 {
+    public static void main(String[] args) {
+        int count= 10;
+        System.out.println(count);
+
+        final double PI = 3.14159;
+        int j = 5;
+        double rs = j * 2 * PI;
+        System.out.println(rs);
+
+    }
+
+    
+}
