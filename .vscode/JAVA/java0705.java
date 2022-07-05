ff40ed31c0175cbee3cf291b82184f47c6404d55
test <test@gmail.com>
2022-07-05 15:06:58 +0900

    m6

--- .vscode/JAVA/java0705.java
+++ .vscode/JAVA/java0705.java
@@ -1,4 +1,4 @@
-public class java0323 {
+public class java0705 {
     public static void main(String[] args) {
         System.out.println("commit 07  - - 05");
     }
--- .vscode/JAVA/java_0323.java
+++ .vscode/JAVA/java_0323.java
@@ -1,5 +1,5 @@
 public class java_0323 {
     public static void main(String[] args) {
-        System.out.println("03-23 commit ver 5");
+        System.out.println("03-23 commit ver 6");
     } 
 }
